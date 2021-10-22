e53c8883434404dd970350799108c5bd6c6f17dd
Hoku113 <hokuto.u0113@gmail.com>
2021-10-23 02:35:25 +0900

    practice

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const config = {
-	channelSecret: "",
-	channelAccessToken: "",
+	channelSecret: "7ae9be46e00471a4867fb1df7e5c675d",
+	channelAccessToken: "UVvcprbcVIdjzMqfe87f981t9O3JMEaqjlpsju0BIGyuNwxx3PjzcgO9OedBF1LxkjU/8qGgLzPhKMgPfnupVz9kg3Bn8gIP+6WlahXZ6WRgRsGf2tpYuHGiiBpwzpGC+Dk9wfsM9hjR99SgqQUg0wdB04t89/1O/w1cDnyilFU=",
 };
 
 const client = new line.Client(config);
@@ -22,13 +22,35 @@ app.post("/webhook", line.middleware(config), async (req, res) => {
 	let result;
 	if (text === "hello") {
 		result = await client.replyMessage(event.replyToken, {
-			type: "text",
-			text: "こんにちは！",
-		});
+			"type": "template",
+			"altText": "This is a buttons template",
+			"template": {
+				"type": "buttons",
+				"title": "Menu",
+				"text": "Please select",
+				"actions": [
+					{
+					  "type": "postback",
+					  "label": "Buy",
+					  "data": "action=buy&itemid=123"
+					},
+					{
+					  "type": "postback",
+					  "label": "Add to cart",
+					  "data": "action=add&itemid=123"
+					},
+					{
+					  "type": "uri",
+					  "label": "View detail",
+					  "uri": "http://example.com/page/123"
+					}
+				]
+			}
+		  });
 	} else {
 		result = await client.replyMessage(event.replyToken, {
 			type: "text",
-			text: `${text}が押されました`,
+			text: `Σ(ﾟДﾟ)`,
 		});
 	}
 
